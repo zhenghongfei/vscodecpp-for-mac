5c523652244b3767c842c4ea646bcc6f8d00866e
zhenghongfei <hongfeizheng@163.com>
2021-08-01 16:12:21 +0800

    update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::string desc = "Hello";
-    std::string lan = "C++";
+    std::string lan = "C++ for mac";
     std::cout << desc << "," << lan << std::endl;
     return 0;
 }
