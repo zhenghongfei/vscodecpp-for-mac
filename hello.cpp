60b40f7b4759a7b90823923b479e7c2a9296ac14
zhenghongfei <hongfeizheng@163.com>
2021-08-01 16:40:11 +0800

    update hello.cpp out

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::string desc = "Hello";
+    std::string desc = "Hello mac";
     std::string lan = "C++";
     std::cout << desc << "," << lan << std::endl;
     return 0;
